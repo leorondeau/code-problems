fcf9ef8b0948cff3d31c1be053a560bdebc2634a
Leo Rondeau <christopherleorondeau@gmail.com>
2021-07-26 22:23:37 -0500

    anagram

--- scripts/raveTech.js
+++ scripts/raveTech.js
@@ -13,6 +13,8 @@
     2 Write a  program to print all permutations of a given String. For example, if given String is "GOD" then your program should print all 6 permutations of this string, 
     e.g. "GOD," "OGD," "DOG," "GDO," "ODG," and "DGO."
 
+
+
     
 
     *write a function that checks if two provided strings are anagrams of each other; letter casing shouldn’t matter. Also, consider only characters, not spaces or punctuation. For example
@@ -20,3 +22,42 @@
     anagram('hello', 'bye') --> false
 
 */
+
+//  const anagram = (wordOne, wordTwo) => {
+//     debugger
+//     const firstWord = wordOne.split('').sort()
+//     const secondWord = wordTwo.split('').sort()
+//     const firstJoin = firstWord.join()
+//     const secondJoin = secondWord.join()
+//     const comp = (firstJoin === secondJoin) ? "anagram" : "not anagram"
+//     return comp
+// }
+//  console.log(anagram('hello','bye'))
+
+const permut = (string) => {
+    // debugger
+    if (string < 2) {
+        return string
+    }
+    let permutations = []
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i]
+        if (string.indexOf(char) != i)
+            continue
+
+        let remainingString = string.slice(0, i) + string.slice(i + 1, string.length); //Note: you can concat Strings via '+' in JS
+
+        for (let subPermutation of permut(remainingString))
+            permutations.push(char + subPermutation)
+    }
+    console.log(permutations)
+    return permutations
+}
+
+
+// const myString = "dog";
+// let permutations = permut(myString);
+// for (permutation of permutations) {
+//     console.log('perm', permutation)
+// }
+
